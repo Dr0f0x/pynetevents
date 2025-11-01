b7172e976fc54e2c5a4adf268abe637a7fef3a97
Dr0f0x <Dr0f0x@outlook.de>
2025-11-01 01:52:06 +0100

    add codecov job

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+"""
+documentation for pynetevents
+"""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
